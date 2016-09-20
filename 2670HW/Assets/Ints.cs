c6f2a8abc4c951499b0baecfc633cf5d46c9567d
Fritz Dittmer <fdittmer9762@gmail.com>
2016-09-20 17:14:46 -0600

    Var Sctipt

---
+++ 2670HW/Assets/Ints.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ints : MonoBehaviour {
+
+	public float firstNumber;
+	public float secondNumber;
+
+	void Start () {
+		Add (firstNumber, secondNumber); //etc
+		Subtract (firstNumber, secondNumber); //etc
+		Multiply (firstNumber, secondNumber); //etc
+		Dev (firstNumber, secondNumber); //etc
+	}
+
+	float Add(float x, float y){
+		float temp = x + y;
+		Debug.Log (x + " + " + y + " = " + temp);
+		return temp;
+	}
+
+	float Subtract(float x, float y){
+		float temp = x - y;
+		Debug.Log (x + " - " + y + " = " + temp);
+		return temp;
+	}
+
+	float Multiply (float x, float y){
+		float temp = x * y;
+		Debug.Log (x + " * " + y + " = " + temp);
+		return temp;
+	}
+
+	float Dev(float x, float y){
+		float temp = x / y;
+		Debug.Log (x + " / " + y + " = " + temp);
+		return temp;
+	}
+}
